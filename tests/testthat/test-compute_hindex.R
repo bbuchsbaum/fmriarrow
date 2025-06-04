15384202ab6afe0958d6cded9708af40609a3265
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-04 04:38:27 -0400

    Add validation for compute_hindex and compute_zindex

--- tests/testthat/test-compute_hindex.R
+++ tests/testthat/test-compute_hindex.R
@@ -18,5 +18,8 @@ test_that("vectorized input works", {
 test_that("input validation", {
   expect_error(compute_hindex(-1, 0, 0))
   expect_error(compute_hindex(0, 0, 1024, max_coord_bits = 10))
+  expect_error(compute_hindex(0.5, 0, 0))
+  expect_error(compute_hindex(0, 0, 0, max_coord_bits = 0))
+  expect_error(compute_hindex(0, 0, 0, max_coord_bits = 31))
 })
 
