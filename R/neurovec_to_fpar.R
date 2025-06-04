0bf6c51245d863f484787c5e91551fa36e4984db
bbuchsbaum <brad.buchsbaum@gmail.com>
2025-06-03 22:16:41 -0400

    Implement CORE-004 voxel extraction

--- R/neurovec_to_fpar.R
+++ R/neurovec_to_fpar.R
@@ -26,6 +26,39 @@ neurovec_to_fpar <- function(neuro_vec_obj, output_parquet_path,
 
   space_obj <- neuroim2::space(neuro_vec_obj)
 
+  dims <- dim(space_obj)
+  if (length(dims) < 3) {
+    stop("NeuroSpace must have at least 3 spatial dimensions")
+  }
+
+  n_vox <- prod(dims[1:3])
+  x <- integer(n_vox)
+  y <- integer(n_vox)
+  z <- integer(n_vox)
+  zindex <- integer(n_vox)
+  bold <- vector("list", n_vox)
+
+  for (i in seq_len(n_vox)) {
+    coords <- neuroim2::index_to_grid(space_obj, i)
+    x[i] <- coords[1] - 1L
+    y[i] <- coords[2] - 1L
+    z[i] <- coords[3] - 1L
+    zindex[i] <- compute_zindex(x[i], y[i], z[i])
+    bold[[i]] <- as.numeric(neuroim2::series(neuro_vec_obj,
+                                             coords[1], coords[2], coords[3]))
+  }
+
+  voxel_data <- data.frame(
+    subject_id = rep(subject_id, n_vox),
+    session_id = rep(session_id, n_vox),
+    task_id = rep(task_id, n_vox),
+    run_id = rep(run_id, n_vox),
+    x = x, y = y, z = z,
+    zindex = zindex,
+    stringsAsFactors = FALSE
+  )
+  voxel_data$bold <- I(bold)
+
   list(
     neuro_vec_obj = neuro_vec_obj,
     output_parquet_path = output_parquet_path,
@@ -33,6 +66,7 @@ neurovec_to_fpar <- function(neuro_vec_obj, output_parquet_path,
     session_id = session_id,
     task_id = task_id,
     run_id = run_id,
-    space = space_obj
+    space = space_obj,
+    voxel_data = voxel_data
   )
 }
